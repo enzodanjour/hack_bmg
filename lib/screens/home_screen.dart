f74caa59c617e33f7ec8ef861e42a520f927161d
VictorTomaz <lvictortomaz@gmail.com>
2020-05-29 20:20:46 -0300

    screens

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  static const id = 'HomeScreen';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      bottomNavigationBar: BottomNavigationBar(
+        items: <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            title: SizedBox(
+              height: 0,
+            ),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.search),
+            title: SizedBox(
+              height: 0,
+            ),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            title: SizedBox(
+              height: 0,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/screens/splash_screen.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:hack_bmg_flutter/screens/home_screen.dart';
+
+class SplashScreen extends StatefulWidget {
+  static const id = 'SplashScreen';
+
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+
+    Navigator.pushNamed(context, HomeScreen.id);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
