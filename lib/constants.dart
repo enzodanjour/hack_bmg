f89d2ece1df4e5e75f6a31915f631fcbf958473a
VictorTomaz <lvictortomaz@gmail.com>
2020-05-29 21:51:50 -0300

    constants

---
+++ lib/constants.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+//Colors
+const kSelectedColor = Color(0xFF000000);
+const kUnselectedColor = Color(0xFFCCCCCC);
+
+//Themes
+const kBottomThemeData = IconThemeData(
+  color: Colors.black,
+  size: 30,
+);
+
+//Widgets
+const kVoidWidget = SizedBox(
+  height: 0,
+);
