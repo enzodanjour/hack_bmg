c227efea060b8d8b6971da9d14f4617ab2e1865d
VictorTomaz <lvictortomaz@gmail.com>
2020-05-30 01:04:12 -0300

    models

---
+++ lib/models/objective.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Objective {
+  final IconData icon;
+  final String label;
+  final double value;
+  final double percent;
+
+  Objective({this.icon, this.label, this.value, this.percent});
+}
