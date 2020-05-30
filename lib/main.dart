8012556408a0ecd99ec9873276c2252aa3b15b18
VictorTomaz <lvictortomaz@gmail.com>
2020-05-30 01:03:27 -0300

    added provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:hack_bmg_flutter/models/objective_data.dart';
 import 'package:hack_bmg_flutter/screens/splash_screen.dart';
 import 'package:hack_bmg_flutter/screens/home_screen.dart';
+import 'package:provider/provider.dart';
 
 void main() {
   runApp(MyApp());
@@ -9,12 +11,15 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      initialRoute: SplashScreen.id,
-      routes: {
-        SplashScreen.id: (context) => SplashScreen(),
-        HomeScreen.id: (context) => HomeScreen(),
-      },
+    return ChangeNotifierProvider(
+      create: (context) => ObjectiveData(),
+      child: MaterialApp(
+        initialRoute: SplashScreen.id,
+        routes: {
+          SplashScreen.id: (context) => SplashScreen(),
+          HomeScreen.id: (context) => HomeScreen(),
+        },
+      ),
     );
   }
 }
