1ad331751c97b12e946a0227cd1241eed75091e5
VictorTomaz <lvictortomaz@gmail.com>
2020-05-29 22:14:29 -0300

    update

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -20,35 +20,46 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(),
-      bottomNavigationBar: BottomNavigationBar(
-        type: BottomNavigationBarType.fixed,
-        selectedIconTheme: kBottomThemeData.copyWith(
-          color: Colors.black,
+      body: ListView(
+        children: <Widget>[],
+      ),
+      bottomNavigationBar: Container(
+        decoration: BoxDecoration(boxShadow: <BoxShadow>[
+          BoxShadow(
+            color: Colors.grey,
+            blurRadius: 15,
+            spreadRadius: 10,
+          ),
+        ]),
+        child: BottomNavigationBar(
+          type: BottomNavigationBarType.fixed,
+          selectedIconTheme: kBottomThemeData.copyWith(
+            color: Colors.black,
+          ),
+          unselectedIconTheme: kBottomThemeData.copyWith(
+            color: Colors.grey,
+          ),
+          items: <BottomNavigationBarItem>[
+            BottomNavigationBarItem(
+              icon: Icon(Icons.account_balance_wallet),
+              title: kVoidWidget,
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.school),
+              title: kVoidWidget,
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.chat_bubble),
+              title: kVoidWidget,
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.person),
+              title: kVoidWidget,
+            ),
+          ],
+          currentIndex: _selectedIndex,
+          onTap: _onItemTapped,
         ),
-        unselectedIconTheme: kBottomThemeData.copyWith(
-          color: Colors.grey,
-        ),
-        items: <BottomNavigationBarItem>[
-          BottomNavigationBarItem(
-            icon: Icon(Icons.account_balance_wallet),
-            title: kVoidWidget,
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.school),
-            title: kVoidWidget,
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.chat_bubble),
-            title: kVoidWidget,
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.person),
-            title: kVoidWidget,
-          ),
-        ],
-        currentIndex: _selectedIndex,
-        onTap: _onItemTapped,
       ),
     );
   }
