7a28d918c000b60291a608113ea3be1b3fcc7347
VictorTomaz <lvictortomaz@gmail.com>
2020-05-29 23:29:38 -0300

    components

---
+++ lib/components/summary_card.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class SummaryCard extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 200,
+      width: double.infinity,
+      decoration: BoxDecoration(
+        color: Color(0xFFD1FFD3),
+        borderRadius: BorderRadius.circular(20.0),
+      ),
+    );
+  }
+}
