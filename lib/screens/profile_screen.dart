1f0fa1d08931690ed2c3d0acf88a095f95a0d544
Enzo Danjour <enzo.lops999@gmail.com>
2020-05-31 13:26:31 -0300

    Scrool

--- lib/screens/profile_screen.dart
+++ lib/screens/profile_screen.dart
@@ -15,220 +15,224 @@ class _ProfileScreenState extends State<ProfileScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-          child: Column(
-          children: [
-            Container(
-              decoration: BoxDecoration(
-                gradient: LinearGradient(
-                  begin: Alignment.topCenter,
-                  end: Alignment.bottomCenter,
-                  colors:[Colors.redAccent,Colors.orangeAccent]
-                )
-              ),
-              child: Container(
-                width: double.infinity,
-                height: 150.0,
-                child: Center(
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Container(
-                        width: 100,
-                        height: 100,
-                        child: GFAvatar(
-                          backgroundImage: NetworkImage('https://www.rd.com/wp-content/uploads/2017/09/01-shutterstock_476340928-Irina-Bg.jpg'),
-                          shape: GFAvatarShape.standard,                      
+      body: 
+      SingleChildScrollView(
+          child: SafeArea(
+            child: Column(
+            children: [
+              Container(
+                decoration: BoxDecoration(
+                  gradient: LinearGradient(
+                    begin: Alignment.topCenter,
+                    end: Alignment.bottomCenter,
+                    colors:[Colors.redAccent,Colors.orangeAccent]
+                  )
+                ),
+                child: Container(
+                  width: double.infinity,
+                  height: 150.0,
+                  child: Center(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Container(
+                          width: 100,
+                          height: 100,
+                          child: GFAvatar(
+                            backgroundImage: NetworkImage('https://www.rd.com/wp-content/uploads/2017/09/01-shutterstock_476340928-Irina-Bg.jpg'),
+                            shape: GFAvatarShape.standard,                      
+                          ),
                         ),
-                      ),
-                    ],
+                      ],
+                    ),
                   ),
                 ),
               ),
-            ),
-            Container(
-              alignment: Alignment.centerLeft,
-              padding: EdgeInsets.all(16.0),
-              child: Text(
-                'Maria Conceição Leopoldino', 
-                style: kTextStyle.copyWith(
-                  fontSize: 36.0,
-                  fontWeight: FontWeight.w400
-                )
-              )
-            ),
-            Container(
-              alignment: Alignment.centerLeft,
-              padding: EdgeInsets.all(10.0),
-              child: Text(
-                'Tipo de experiência', 
-                style: kTextStyle.copyWith(
-                  fontSize: 24.0,
-                  fontWeight: FontWeight.w400
+              Container(
+                alignment: Alignment.centerLeft,
+                padding: EdgeInsets.all(16.0),
+                child: Text(
+                  'Maria Conceição Leopoldino', 
+                  style: kTextStyle.copyWith(
+                    fontSize: 36.0,
+                    fontWeight: FontWeight.w400
+                  )
                 )
               ),
-            ),
-            Container(
-              height: 10,
-            ),
-             Row(
-               mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text(
-                  'Simples', 
+              Container(
+                alignment: Alignment.centerLeft,
+                padding: EdgeInsets.all(10.0),
+                child: Text(
+                  'Tipo de experiência', 
+                  style: kTextStyle.copyWith(
+                    fontSize: 24.0,
+                    fontWeight: FontWeight.w400
+                  )
+                ),
+              ),
+              Container(
+                height: 10,
+              ),
+               Row(
+                 mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    'Simples', 
+                    style: kTextStyle.copyWith(
+                      fontSize: 22.0,
+                      fontWeight: FontWeight.w400
+                    )
+                  ),
+                  Switch(
+                    value: avancado, 
+                    onChanged: (value){
+                      setState(() {
+                        avancado = value;
+                      });
+                    },
+                    activeTrackColor: Colors.green,
+                    activeColor:Colors.green,
+                  ),
+                  Text(
+                    'Avançada', 
+                    style: kTextStyle.copyWith(
+                      fontSize: 22.0,
+                      fontWeight: FontWeight.w400
+                    )
+                  ),
+                ],
+              ),
+              Container(
+                alignment: Alignment.centerLeft,
+                padding: EdgeInsets.all(16.0),
+                child: avancado ? Text(
+                  'No modo avançado você poupa e também investe. Esse modo é indicado para quem deseja aprender a investir o seu dinheiro e aumentar o patrimônio. Invista com baixo ou alto risco.', 
+                  style: kTextStyle.copyWith(
+                    fontSize: 16.0,
+                    fontWeight: FontWeight.w200
+                  )
+                ) : Text(
+                  'No modo simples você apenas poupa o seu dinheiro. Esse modo é indicado para quem deseja aprender a gerir melhor o seu dinheiro, sem nenhum risco.', 
+                  style: kTextStyle.copyWith(
+                    fontSize: 16.0,
+                    fontWeight: FontWeight.w200
+                  )
+                ), 
+              ),
+              Container(
+                alignment: Alignment.centerLeft,
+                padding: EdgeInsets.all(16.0),
+                child: Text(
+                  'Distribuição do seu dinheiro', 
                   style: kTextStyle.copyWith(
                     fontSize: 22.0,
                     fontWeight: FontWeight.w400
                   )
                 ),
-                Switch(
-                  value: avancado, 
-                  onChanged: (value){
-                    setState(() {
-                      avancado = value;
-                    });
-                  },
-                  activeTrackColor: Colors.green,
-                  activeColor:Colors.green,
-                ),
-                Text(
-                  'Avançada', 
-                  style: kTextStyle.copyWith(
-                    fontSize: 22.0,
-                    fontWeight: FontWeight.w400
-                  )
-                ),
-              ],
-            ),
-            Container(
-              alignment: Alignment.centerLeft,
-              padding: EdgeInsets.all(16.0),
-              child: avancado ? Text(
-                'No modo avançado você poupa e também investe. Esse modo é indicado para quem deseja aprender a investir o seu dinheiro e aumentar o patrimônio. Invista com baixo ou alto risco.', 
-                style: kTextStyle.copyWith(
-                  fontSize: 16.0,
-                  fontWeight: FontWeight.w200
-                )
-              ) : Text(
-                'No modo simples você apenas poupa o seu dinheiro. Esse modo é indicado para quem deseja aprender a gerir melhor o seu dinheiro, sem nenhum risco.', 
-                style: kTextStyle.copyWith(
-                  fontSize: 16.0,
-                  fontWeight: FontWeight.w200
-                )
-              ), 
-            ),
-            Container(
-              alignment: Alignment.centerLeft,
-              padding: EdgeInsets.all(16.0),
-              child: Text(
-                'Distribuição do seu dinheiro', 
-                style: kTextStyle.copyWith(
-                  fontSize: 22.0,
-                  fontWeight: FontWeight.w400
-                )
               ),
-            ),
-            Row(
-              crossAxisAlignment: CrossAxisAlignment.end,
-              children: [
-                Expanded(child: Container()),
-                Container(
-                  height: 140,
-                  width: 71,
-                  decoration: BoxDecoration(
-                    color: Colors.red[200],
-                    borderRadius: BorderRadius.vertical(
-                      top: Radius.circular(10),
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: [
+                  Expanded(child: Container()),
+                  Container(
+                    height: 140,
+                    width: 71,
+                    decoration: BoxDecoration(
+                      color: Colors.red[200],
+                      borderRadius: BorderRadius.vertical(
+                        top: Radius.circular(10),
+                      ),
+                      boxShadow: [
+                        // to make elevation
+                        BoxShadow(
+                          color: Colors.black45,
+                          offset: Offset(2, 2),
+                          blurRadius: 4,
+                        ),
+                        // to make the coloured border
+                        BoxShadow(
+                          color: Colors.red[600],
+                          offset: Offset(0, 4),
+                        ),
+                      ],
                     ),
-                    boxShadow: [
-                      // to make elevation
-                      BoxShadow(
-                        color: Colors.black45,
-                        offset: Offset(2, 2),
-                        blurRadius: 4,
-                      ),
-                      // to make the coloured border
-                      BoxShadow(
-                        color: Colors.red[600],
-                        offset: Offset(0, 4),
-                      ),
-                    ],
                   ),
-                ),
-                Expanded(child: Container()),
-                Container(
-                  height: 126,
-                  width: 71,
-                  decoration: BoxDecoration(
-                    color: Colors.blue[400],
-                    borderRadius: BorderRadius.vertical(
-                      top: Radius.circular(10),
+                  Expanded(child: Container()),
+                  Container(
+                    height: 126,
+                    width: 71,
+                    decoration: BoxDecoration(
+                      color: Colors.blue[400],
+                      borderRadius: BorderRadius.vertical(
+                        top: Radius.circular(10),
+                      ),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.black45,
+                          offset: Offset(2, 2),
+                          blurRadius: 4,
+                        ),
+                        // to make the coloured border
+                        BoxShadow(
+                          color: Colors.blue[900],
+                          offset: Offset(0, 4),
+                        ),
+                      ],
                     ),
-                    boxShadow: [
-                      BoxShadow(
-                        color: Colors.black45,
-                        offset: Offset(2, 2),
-                        blurRadius: 4,
-                      ),
-                      // to make the coloured border
-                      BoxShadow(
-                        color: Colors.blue[900],
-                        offset: Offset(0, 4),
-                      ),
-                    ],
                   ),
-                ),
-                Expanded(child: Container()),
-                Container(
-                  height: 104,
-                  width: 71,
-                  decoration: BoxDecoration(
-                    color: Colors.yellow[300],
-                    borderRadius: BorderRadius.vertical(
-                      top: Radius.circular(10),
+                  Expanded(child: Container()),
+                  Container(
+                    height: 104,
+                    width: 71,
+                    decoration: BoxDecoration(
+                      color: Colors.yellow[300],
+                      borderRadius: BorderRadius.vertical(
+                        top: Radius.circular(10),
+                      ),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.black45,
+                          offset: Offset(2, 2),
+                          blurRadius: 4,
+                        ),
+                        // to make the coloured border
+                        BoxShadow(
+                          color: Colors.yellow[600],
+                          offset: Offset(0, 4),
+                        ),
+                      ],
                     ),
-                    boxShadow: [
-                      BoxShadow(
-                        color: Colors.black45,
-                        offset: Offset(2, 2),
-                        blurRadius: 4,
-                      ),
-                      // to make the coloured border
-                      BoxShadow(
-                        color: Colors.yellow[600],
-                        offset: Offset(0, 4),
-                      ),
-                    ],
                   ),
-                ),
-                Expanded(child: Container()),
-                Container(
-                  height: 70,
-                  width: 71,
-                  decoration: BoxDecoration(
-                    color: Colors.grey[400],
-                    borderRadius: BorderRadius.vertical(
-                      top: Radius.circular(10),
+                  Expanded(child: Container()),
+                  Container(
+                    height: 70,
+                    width: 71,
+                    decoration: BoxDecoration(
+                      color: Colors.grey[400],
+                      borderRadius: BorderRadius.vertical(
+                        top: Radius.circular(10),
+                      ),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.black45,
+                          offset: Offset(2, 2),
+                          blurRadius: 4,
+                        ),
+                        // to make the coloured border
+                        BoxShadow(
+                          color: Colors.grey[800],
+                          offset: Offset(0, 4),
+                        ),
+                      ],
                     ),
-                    boxShadow: [
-                      BoxShadow(
-                        color: Colors.black45,
-                        offset: Offset(2, 2),
-                        blurRadius: 4,
-                      ),
-                      // to make the coloured border
-                      BoxShadow(
-                        color: Colors.grey[800],
-                        offset: Offset(0, 4),
-                      ),
-                    ],
                   ),
-                ),
-                Expanded(child: Container()),
-              ],
-            )
-          ], 
+                  Expanded(child: Container()),
+                ],
+              ),
+                
+            ], 
+          ),
         ),
       ),
     );
